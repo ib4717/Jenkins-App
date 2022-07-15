2efaa1d9d250d74e7ebbfa29ede67d53dbf48724
ib4717 <ib4717@srmist.edu.in>
2022-07-15 15:37:14 +0530

    updated

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -1,12 +1,37 @@
-﻿namespace JenkinsApp
+﻿using System;
+using MyMaths;
+
+namespace JenkinsApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome to Jenkins");
-            Console.ReadLine();
+            Console.Write("Enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Press 1 to add or 2 to multiply");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+            MyMaths.MyMaths math = new MyMaths.MyMaths();
+            if(option == 1)
+            {
+                result = math.Add(a, b);
+                Console.WriteLine(a + " + " + b + " = " + result);
+            }
+            else if(option == 2)
+            {
+                result = math.Mul(a, b);
+                Console.WriteLine(a + " * " + b + " = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
         }
     }
 }
---
+++ MyMaths/MyMaths.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMaths
+{
+    public class MyMaths
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
