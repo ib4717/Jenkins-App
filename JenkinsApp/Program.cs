e3d335977c0c18eea8019687d64c214f01d93139
ib4717 <ib4717@srmist.edu.in>
2022-07-15 11:48:51 +0530

    Commit

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Console.ReadLine();
+        }
+    }
+}
