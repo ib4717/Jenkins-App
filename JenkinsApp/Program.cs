30f908bb5542c99f16e64885aff0309a95267121
ib4717 <ib4717@srmist.edu.in>
2022-07-15 12:51:11 +0530

    c

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -4,11 +4,9 @@
     {
         static void Main(string[] args)
         {
-            string name;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins");
-            name = Console.ReadLine();
-            Console.WriteLine("Welcome " + name);
+            Console.ReadLine();
         }
     }
 }
